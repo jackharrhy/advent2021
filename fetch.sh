1f3f5655ec6ff4d58e4cce6ff61d2231caf30edd
Jack Arthur Harrhy <me@jackharrhy.com>
2021-12-03 21:19:00 -0330

    move to livebook, day 1, 2, & 3

---
+++ fetch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# fetch livebook files from my server
+rsync "jack@jackharrhy.dev:~/jackharrhy.com-ops/core/volumes/livebook/advent2021/*" .
--- lib/colorize.ex
+++
@@ -1,7 +0,0 @@
-defmodule Advent2021.Colorize do
-  def colorize(escape, string) do
-    [escape, string, :reset]
-    |> IO.ANSI.format_fragment(true)
-    |> IO.iodata_to_binary()
-  end
-end
