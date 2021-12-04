27228704bcf5c79bca847dc1ba71c5f7ed95ebbf
Jack Arthur Harrhy <me@jackharrhy.com>
2021-12-04 03:32:10 -0330

    day 4, cleaned day 3, better fetch script

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+
 # fetch livebook files from my server
+
 rsync "jack@jackharrhy.dev:~/jackharrhy.com-ops/core/volumes/livebook/advent2021/*" .
+
+for file in *.livemd; do
+  [ -e "$file" ] || [ -L "$file" ] || continue
+
+  { echo "<!-- vim: syntax=markdown -->\n" & cat "$file"; } > "new_$file"
+
+  mv "new_$file" "$file"
+done
